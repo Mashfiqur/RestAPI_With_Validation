0001023db79671300f70ba7ec4ea43cbe9919c46
mashfiqur <mashfiqurrr@gmail.com>
2021-04-20 17:35:33 +0600

    bug fixed

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -28,3 +28,101 @@ exports.get_all_orders =  (req,res,next) =>{
         });
     });
 };
+
+exports.create_order = (req,res,next) =>{
+    const order = new Order({
+        _id: new mongoose.Types.ObjectId(),
+        product: req.body.productId,
+        quantity: req.body.quantity
+    });
+    order
+    .save()
+    .then(()=>{
+        res.status(201).json({
+            message: "Order has been added successfully!",
+            createdProduct: order
+        });
+    })
+    .catch((err)=>{
+        res.status(400).json({
+            message: "Bad Request!",
+            error: err
+        });
+    });
+};
+
+
+exports.get_order = (req,res,next) =>{
+    const id = req.params.orderId;
+    Order.findById(id)
+    // .select('_id product quantity')
+    .exec()
+    .then((doc)=>{
+        if(doc){
+            res.status(200).json({
+                message: "Order has been retrieved successfully!",
+                Order: doc
+            });
+        }
+        else{
+            res.status(400).json({
+                message: "No valid result for this particular object ID!",
+            });
+        }
+        
+    }).catch((err)=>{
+        res.status(500).json({
+            message: "Internal Server Error!",
+            error : err
+        });
+    });
+};
+
+
+exports.update_order = (req,res,next) =>{
+    const id = req.params.orderId;
+    // res.json(req.body);
+    Order.findOneAndUpdate({_id:id},req.body,{upsert: true})
+    .exec()
+    .then((doc)=>{
+        res.status(200).json({
+            message: "Order has been updated successfully!",
+            Order: doc
+        });
+    })
+    .catch((err)=>{
+        res.status(500).json({
+            message: "Internal Server Error!",
+            error : err
+        });
+    });
+};
+
+exports.remove_order = (req,res,next) =>{
+    const id = req.params.orderId;
+    Order.remove({_id:id})
+    .exec()
+    .then((doc)=>{
+        if(doc){
+            res.status(200).json({
+                message: "Order has been removed successfully!",
+                request:{
+                    type: "POST",
+                    url: "/orders",
+                    body:'{"name":String, "sku": String,"price":Number,"currency":String}'
+                }
+            });
+        }
+        else{
+            res.status(400).json({
+                message: "No valid result for this particular object ID!"
+            });
+        }
+        
+    }).catch((err)=>{
+        res.status(500).json({
+            message: "Internal Server Error!",
+            error : err
+        });
+    });
+};
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
-const checkAuth = require('../middleware/check-auth')
+const checkAuth = require('../middleware/check-auth');
 
 const OrderController =  require('../controllers/orders');
 
@@ -9,100 +9,12 @@ const Order = require('../models/order');
 
 router.get('/', checkAuth, OrderController.get_all_orders);
 
-router.post('/', (req,res,next) =>{
-    const order = new Order({
-        _id: new mongoose.Types.ObjectId(),
-        product: req.body.productId,
-        quantity: req.body.quantity
-    });
-    order
-    .save()
-    .then(()=>{
-        res.status(201).json({
-            message: "Order has been added successfully!",
-            createdProduct: order
-        });
-    })
-    .catch((err)=>{
-        res.status(400).json({
-            message: "Bad Request!",
-            error: err
-        });
-    });
-});
+router.post('/', checkAuth, OrderController.create_order);
 
-router.get('/:orderId', (req,res,next) =>{
-    const id = req.params.orderId;
-    Order.findById(id)
-    // .select('_id product quantity')
-    .exec()
-    .then((doc)=>{
-        if(doc){
-            res.status(200).json({
-                message: "Order has been retrieved successfully!",
-                Order: doc
-            });
-        }
-        else{
-            res.status(400).json({
-                message: "No valid result for this particular object ID!",
-            });
-        }
-        
-    }).catch((err)=>{
-        res.status(500).json({
-            message: "Internal Server Error!",
-            error : err
-        });
-    });
-});
+router.get('/:orderId', checkAuth, OrderController.get_order);
 
-router.patch('/:orderId', (req,res,next) =>{
-    const id = req.params.orderId;
-    // res.json(req.body);
-    Order.findOneAndUpdate({_id:id},req.body,{upsert: true})
-    .exec()
-    .then((doc)=>{
-        res.status(200).json({
-            message: "Order has been updated successfully!",
-            Order: doc
-        });
-    })
-    .catch((err)=>{
-        res.status(500).json({
-            message: "Internal Server Error!",
-            error : err
-        });
-    });
-});
+router.patch('/:orderId', checkAuth, OrderController.update_order);
 
-router.delete('/:orderId', (req,res,next) =>{
-    const id = req.params.orderId;
-    Order.remove({_id:id})
-    .exec()
-    .then((doc)=>{
-        if(doc){
-            res.status(200).json({
-                message: "Order has been removed successfully!",
-                request:{
-                    type: "POST",
-                    url: "/orders",
-                    body:'{"name":String, "sku": String,"price":Number,"currency":String}'
-                }
-            });
-        }
-        else{
-            res.status(400).json({
-                message: "No valid result for this particular object ID!"
-            });
-        }
-        
-    }).catch((err)=>{
-        res.status(500).json({
-            message: "Internal Server Error!",
-            error : err
-        });
-    });
-});
+router.delete('/:orderId', checkAuth, OrderController.remove_order);
 
 module.exports = router;
